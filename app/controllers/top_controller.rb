babc8cb01e995363d41d4b9476cf6dfc7fd2f276
xiangyang <xiang.yang@maisiteng.com>
2016-06-03 13:05:53 +0800

    user

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,6 @@
+class TopController < ActionController::Base
+
+  def index
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #You can have the root of your site routed with "root"
+  root 'top#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
