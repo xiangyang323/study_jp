e943ea1f962b0c9a9e910c77293d846324126e21
xiangyang <xiang.yang@maisiteng.com>
2016-06-27 23:34:28 +0800

    添加辞典

--- app/models/word.rb
+++ app/models/word.rb
@@ -6,8 +6,16 @@ class Word < ActiveRecord::Base
   CLASSIFY_N = "n"  #none
   CLASSIFY_PRO = "pro" #pronone
   CLASSIFY_ADE = "ade" #adverbial
-  CLASSIFY = Hash[CLASSIFY_N, "名词", CLASSIFY_PRO, "代词", CLASSIFY_ADE, "副词"]
+  CLASSIFY_V1 = "v1"
+  CLASSIFY_V2 = "v2"
+  CLASSIFY_V3 = "v3"
+  CLASSIFY = Hash[CLASSIFY_N, "名词", CLASSIFY_PRO, "代词", CLASSIFY_ADE, "副词",
+  	CLASSIFY_V1, "动1", CLASSIFY_V2, "动2", CLASSIFY_V3, "动3"]
 
+  #local
   LOCAL_1_1 = "1_1"
+  LOCAL_1_2 = "1_2"
+  LOCAL_1_3 = "1_3"
+  LOCAL_1_4 = "1_4"
   
 end
