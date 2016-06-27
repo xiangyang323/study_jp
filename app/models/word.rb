f03b295757a1879f04d3be61fb6e7041ebcd5d6a
xiangyang <xiang.yang@maisiteng.com>
2016-06-27 18:46:00 +0800

    添加batch

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,6 +1,13 @@
 class Word < ActiveRecord::Base
   has_many :word_r_categories
 
+ 
   #classify
-  CLASSIFY = Hash
+  CLASSIFY_N = "n"  #none
+  CLASSIFY_PRO = "pro" #pronone
+  CLASSIFY_ADE = "ade" #adverbial
+  CLASSIFY = Hash[CLASSIFY_N, "名词", CLASSIFY_PRO, "代词", CLASSIFY_ADE, "副词"]
+
+  LOCAL_1_1 = "1_1"
+  
 end
